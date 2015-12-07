8720cb1d9d60861e9dfa588910a8986c9f38c2bd
simsarulhaqv <simsarulhaqv@gmail.com>
2015-12-07 22:01:29 +0530

    Added mailBroadcast()

--- Hwacha/broadcast.py
+++ Hwacha/broadcast.py
@@ -1,4 +1,7 @@
 import tweepy
+import smtplib
+
+from email.mime.text import MIMEText
 
 class Broadcast(object): #Abstract class 
     
@@ -35,8 +38,26 @@ class TwitterBroadcast(Broadcast): #concrete class for twitter
             
 
 
-class Mailbroadcaster(Broadcast): #dummy mail concrete class
-    pass
+class mailBroadcast(Broadcast): #dummy mail concrete class
+
+		def __init__(self,MESSAGE,SUBJECT,FROM,TO) :
+			self.MESSAGE = MESSAGE
+			self.SUBJECT = SUBJECT
+			self.FROM = FROM
+			self.TO = TO
+
+    	def push(self):
+			msg = MIMEText(self.MESSAGE)
+			msg['Subject'] = self.SUBJECT
+			msg['From'] = self.FROM
+			msg['To'] = self.TO 
+
+			#Sending the message via once own SMTP server
+			sendObject = smtplib.SMTP('localhost')
+			sendObject.sendmail(self.FROM,[self.TO], msg.as_string())
+			sendObject.quit()
+
+			# catching exceptions to be done
 
 
 
@@ -46,7 +67,7 @@ def init_twitter(message,key): # twitter key initialisation and broadcasting
     consumer_key = key['consumer_key']
     consumer_secret = key['consumer_secret']
     access_token = key['access_token']
-    access_token_secret = key['access_token_secret']
+    access_token_secret = key['acces s_token_secret']
 
     twitter=TwitterBroadcast(consumer_key,consumer_secret,access_token,access_token_secret)
     key=twitter.authentication()
@@ -54,8 +75,15 @@ def init_twitter(message,key): # twitter key initialisation and broadcasting
     return code
 
 
-def init_mail(): #dummy mail initialisation
-    pass
+def init_mail(message,key): #dummy mail initialisation
+    
+	fromAddress = key['from']
+	toAddress = key['to']
+	subject = key['subject']
+
+	mail = mailBroadcast(message,subject,fromAddress,toAddress)
+	sendMailStatus = mail.push()
+	return sendMailStatus
 
 
 
