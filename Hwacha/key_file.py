81fb7ce386f557631b7e4902ae5bbf70acdedc92
shamlik <tshamlikt@gmail.com>
2015-12-08 11:26:12 +0530

    updated key_file

---
+++ Hwacha/key_file.py
@@ -0,0 +1,14 @@
+#configuration key file for SM
+
+
+#twitter  configuration keys
+consumer_key = 'igjX6vqB8HtMzPByguX9KUtqv'
+consumer_secret= 'R8pyvLiKSDDTjyf84DnKVgM4IQLYTXq9fDdCbsq3vCEzXkC7qz'
+access_token = '4472517314-QveTAuRwPQ0JKnBTkVobZZTG3KusZm8fFBUSXKw'
+access_token_secret ='XItUgPH1VCP027HufEewBqdO45dCEz4Ga4IwqOmgo2brN'
+
+#mail configuration keys
+subject = 'Test Subject'
+to ='abc@xyz.in'
+from_='Hwachatest@mydomain.in'
+	
