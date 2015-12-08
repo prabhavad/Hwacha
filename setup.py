2ac073850d63b6115c74e6f4dbab277b8951d198
shamlik <tshamlikt@gmail.com>
2015-12-08 19:34:43 +0530

    solved issue #8

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup( name ='Hwacha',
+       version = '0.1',
+       description = ' Message broadcasting system',
+       url ='https://github.com/shamlikt/Hwacha',
+       author = 'Team Hwacha',
+       author_email='Hwacha_11@gmail.com',
+       license='BSD',
+       packages=['Hwacha'],
+       install_requires=['tweepy',],
+       zip_safe=False
+       )
