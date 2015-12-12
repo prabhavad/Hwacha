f4d4d51928cc0183cd319bb35712476ed8889263
simsarulhaqv <simsarulhaqv@gmail.com>
2015-12-12 15:47:18 +0530

    Add appControl layout

---
+++ Hwacha/appControl/appControl.py
@@ -0,0 +1,22 @@
+# App controller layer
+
+class appController(object): # concrete class
+
+    def getSmName(self,raw_input):
+        smName = raw_input()
+        return smName
+
+    def getSmUserName(self):
+        pass
+
+    def getSmUserPasswd(self):
+        pass
+
+    def getMessage(self):
+        pass
+
+    def getSmList(self):
+        pass
+
+    def isInSmList(self):
+        pass
