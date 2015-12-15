c825dd9088da5c6780822f65ffac47f5440684fe
simsarulhaqv <simsarulhaqv@gmail.com>
2015-12-15 19:59:49 +0530

    Add comboBox for Listing Social Media to Qt4 interface

--- Hwacha/systemAppUI.py
+++ Hwacha/systemAppUI.py
@@ -5,22 +5,32 @@ class hwachaForm(QtGui.QDialog):
 
     # Top level window with no parent
     def __init__(self, parent=None):
+
         # Super to initialize the form
         super(hwachaForm, self).__init__(parent)
+
         self.browser = QtGui.QTextBrowser()
         self.lineEdit = QtGui.QLineEdit("Message")
         # select all, so that user can overwrite
         self.lineEdit.selectAll()
-        self.button =QtGui.QPushButton("Send")
+        self.button = QtGui.QPushButton("Send")
+        self.smComboBox = QtGui.QComboBox()
+        self.smComboBox.addItems(["Twitter","Mail"])
+
         layout = QtGui.QVBoxLayout()
+
         # add widgets to the layout
         layout.addWidget(self.browser)
         layout.addWidget(self.lineEdit)
+        layout.addWidget(self.smComboBox)
         layout.addWidget(self.button)
+
         # setLayout() is the layout manager, which gives ownership of the widgets and of itself to the form, and takes ownership of any nested layouts itself.
         self.setLayout(layout)
+
         # set focus to the start of lineEdit
         self.lineEdit.setFocus()
+
         # To signal the press of the return
         self.connect(self.lineEdit, QtCore.SIGNAL("returnPressed()"), self.updateUi)
         self.connect(self.button, QtCore.SIGNAL("clicked()"), self.updateUi)
