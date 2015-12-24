021e86c081fea1b4b0ae9dbdd9ce8769a27fe839
simsarulhaqv <simsarulhaqv@gmail.com>
2015-12-24 23:01:39 +0530

    add documentations to the system UI code

--- Hwacha/systemAppUI.py
+++ Hwacha/systemAppUI.py
@@ -1,3 +1,5 @@
+# System app UI
+
 import sys
 from appControl import appControl
 from PyQt4 import QtGui,QtCore
@@ -7,10 +9,10 @@ class hwachaForm(QtGui.QDialog):
     # Top level window with no parent
     def __init__(self, parent=None):
 
-        # Super to initialize the form
+        # --------- Super to initialize the form ---------------
         super(hwachaForm, self).__init__(parent)
 
-        # Some data abstractions
+        # --------- Some data abstractions ----------------
         self.defaultMessage = "Message"
         self.send = "Send"
         self.clear = "Clear screen"
@@ -19,6 +21,7 @@ class hwachaForm(QtGui.QDialog):
         self.requiredSmList = []
         self.key = []
 
+        # ----------- Add GUI elements --------------------
         self.browser = QtGui.QTextBrowser()
         self.lineEdit = QtGui.QLineEdit(self.defaultMessage)
         # Select all, so that user can overwrite
@@ -27,40 +30,43 @@ class hwachaForm(QtGui.QDialog):
         self.smComboBox = QtGui.QComboBox()
         self.smComboBox.addItem(self.emptySm)
         self.clearButton = QtGui.QPushButton(self.clear)
-
         #create app object
         appObject = appControl.appController()
         self.smComboBox.addItems(appObject.getAvailableSmList())
         self.lineEdit2 = QtGui.QLineEdit(self.defaultSmName)
-        #self.browser2 = QtGui.QTextBrowser()
         
 
+        # -------- Add layout --------------------------
         layout = QtGui.QVBoxLayout()
 
-        # Add widgets to the layout
+
+        # ---------- Add widgets to the layout --------------
         layout.addWidget(self.browser)
         layout.addWidget(self.lineEdit)
         layout.addWidget(self.smComboBox)
-        #layout.addWidget(self.browser2)
-        # lineEdit2 is purposefully removed from layout
+        # lineEdit2 is purposefully removed from layout, if need to see uncomment
         #layout.addWidget(self.lineEdit2)
         layout.addWidget(self.button)
         layout.addWidget(self.clearButton)
-
         # setLayout() is the layout manager, which gives ownership of the widgets and of itself to the form, and takes ownership of any nested layouts itself.
         self.setLayout(layout)
-
         # Set focus to the start of lineEdit
         self.lineEdit.setFocus()
 
+        
+        # ------------- Add signals for the buttons on an action ------------------
+
         # To signal the press of the return
         self.connect(self.lineEdit, QtCore.SIGNAL("returnPressed()"), self.updateUi)
+
         # To signal the click over the button
         self.connect(self.button, QtCore.SIGNAL("clicked()"), self.updateUi)
         self.connect(self.button, QtCore.SIGNAL("clicked()"), self.resetRequiredSmList)
+        # The underlying is commented, this may come into use in future.
         #self.connect(self.button, QtCore.SIGNAL("clicked()"), self.getKey)
-        #self.connect(self.button, QtCore.SIGNAL("clicked()"), self.broadcast)
+        self.connect(self.button, QtCore.SIGNAL("clicked()"), self.broadcast)
 
+        # clear the screen
         self.connect(self.clearButton, QtCore.SIGNAL("clicked()"), self.cleanBrowser)
 
         # To signal index change in comboBox
@@ -68,28 +74,40 @@ class hwachaForm(QtGui.QDialog):
         self.connect(self.smComboBox, QtCore.SIGNAL("currentIndexChanged(QString)"), self.updateSmBrowser)
         self.connect(self.smComboBox, QtCore.SIGNAL("currentIndexChanged(QString)"), self.updateSmList)
 
+        # set window title
         self.setWindowTitle("Hwacha")
 
+
+    # Broadcast method
     def broadcast(self):
+        """ Broadcast the input message in various social medias specified by the user"""
         message = unicode(self.lineEdit.text())
         appObject = appControl.appController()
         try:
             retValue = appObject.broadcastMessage(message,self.requiredSmList)
+            self.browser.append("<b>%s</b> broadcasted in <font color=blue><b>%s</b></font>" % (text,smName))
         except:
             retValue = "Failed"
+            self.browser.append("<font color=Red>Sending failed</font>")
+
 
     def resetRequiredSmList(self):
+        """ Reset the Social Media list into empty list """
         self.requiredSmList = []
 
     def updateSmList(self):
+        """ Update the social media list.
+        Add the user specified social media into the social media list """
         smName = unicode(self.lineEdit2.text())
         if smName not in self.requiredSmList:
             self.requiredSmList.append(smName)
 
     def cleanBrowser(self):
+        """ Clear the browser screen """
         self.browser.clear()
 
     def updateSmBrowser(self):
+        """ Update the browser screen on the user input on social media """
         #self.browser2.append("<font color=yellow>=)Hwacha=)</font>")
         try:
             smName = unicode(self.lineEdit2.text())
@@ -105,6 +123,7 @@ class hwachaForm(QtGui.QDialog):
         #self.browser2.append("<font color=yellow>(=Hwacha(=</font>")
 
     def updateUi(self):
+        """ Update the browser screen based on the user input and report appropriate errors """
         self.browser.append("<font color=yellow>=)Hwacha=)</font>")
         try:
             text = unicode(self.lineEdit.text())
@@ -115,9 +134,9 @@ class hwachaForm(QtGui.QDialog):
             retValue2 = appObject.getSmName(lambda: smName)
             if retValue == text: 
                 if retValue2 == smName:
-                    if retValue2 != self.defaultSmName and retValue2 != self.emptySm: 
-                        self.browser.append("<b>%s</b> broadcasted in <font color=blue><b>%s</b></font>" % (text,smName))
-                    else:
+                    if retValue2 == self.defaultSmName or retValue2 == self.emptySm: 
+                       # self.browser.append("<b>%s</b> broadcasted in <font color=blue><b>%s</b></font>" % (text,smName))
+                    #else:
                         self.browser.append("<font color=red>Social Media not choosen</font>")
                         raise ValueError('Social Media not choosen')
                 else:
@@ -130,7 +149,9 @@ class hwachaForm(QtGui.QDialog):
             self.browser.append("<font color=red>Error</font>")
         self.browser.append("<font color=yellow>(=Hwacha(=</font>")
 
-app = QtGui.QApplication(sys.argv)
-form = hwachaForm()
-form.show()
-sys.exit(app.exec_())
+
+if __name__ == '__main__' :
+    app = QtGui.QApplication(sys.argv)
+    form = hwachaForm()
+    form.show()
+    sys.exit(app.exec_())
