92b40a3ad628862306447ac5012dd613f126a83b
shamlik <tshamlikt@gmail.com>
2016-01-02 14:22:40 +0530

    add test for terminalUi.addSm, using None and mock

--- test/test_terminal.py
+++ test/test_terminal.py
@@ -59,3 +59,28 @@ def test_removeSocialMedia():
     
     appObject.getAvailableSmList = org_get
     appObject.removeSm = org_rm
+
+
+def test_removeSocialMedia():
+    
+    mock_list = mock.Mock()
+    mock_add = mock.Mock()
+    availableList = ['twitter']
+    mock_list.return_value = availableList
+    appObject=appControl.appController()
+
+    org_get = appObject.getAvailableSmList
+    appObject.getAvailableSmList = mock_list
+    org_rm = appObject.removeSm
+    appObject.addSm = mock_add
+    return_value= ui.removeSocialMedia(['mail'])
+
+    assert appObject.addSm.called_with(['mail'])
+    assert return_value == None
+
+    return_value= ui.removeSocialMedia(['twitter'])
+    assert appObject.addSm.called_with([])
+    assert return_value == None
+    
+    appObject.getAvailableSmList = org_get
+    appObject.addSm = org_rm
