b85d90c453a4cd2b405e0bd39d1632b7e4b807b0
simsarulhaq_b130461cs <you@example.com>
2016-01-09 21:18:27 +0530

    Update key_file

--- Hwacha/appControl/socialMediaControl/key_file.py
+++ Hwacha/appControl/socialMediaControl/key_file.py
@@ -4,12 +4,12 @@
 #twitter  configuration keys
 
 key ={
-'twitter':{'consumer_key' : 'igjX6vqB8HtMzPByguX9KUtqv',
-           'consumer_secret':'R8pyvLiKSDDTjyf84DnKVgM4IQLYTXq9fDdCbsq3vCEzXkC7qz',
-           'access_token':'4472517314-QveTAuRwPQ0JKnBTkVobZZTG3KusZm8fFBUSXKw',
-           'access_token_secret':'XItUgPH1VCP027HufEewBqdO45dCEz4Ga4IwqOmgo2brN'},
+'twitter':{'consumerKey' : 'igjX6vqB8HtMzPByguX9KUtqv',
+           'consumerSecret':'R8pyvLiKSDDTjyf84DnKVgM4IQLYTXq9fDdCbsq3vCEzXkC7qz',
+           'accessToken':'4472517314-QveTAuRwPQ0JKnBTkVobZZTG3KusZm8fFBUSXKw',
+           'accessTokenSecret':'XItUgPH1VCP027HufEewBqdO45dCEz4Ga4IwqOmgo2brN'},
 'mail':{'subject':'Test Subject', 
         'to':'simsarulhaqv@gmail.com',
-        'consumer_key':'simsar012smtp@gmail.com', 
-        'consumer_secret':'newPass295'}
+        'consumerKey':'simsar012smtp@gmail.com', 
+        'consumerSecret':'newPass295'}
      }
